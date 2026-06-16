68339d77693802413883d3a93d4e7b4847dfccd4
yasharkind <yasharkind95@gmail.com>
2026-06-16 16:28:55 +0330

    chore: missed some embeds for ;;cfg that i added now

--- commands/config.py
+++ commands/config.py
@@ -1,6 +1,5 @@
 import logging
 from attrs import asdict, has
-from discord import Embed
 from discord.ext import commands
 
 from base.config import Config, GuildConfig
@@ -24,15 +23,15 @@ class ConfigCommand(commands.Cog):
 
         match args:
             case ("check", *_):
-                await ctx.send(self.check(cfg))
+                await ctx.send(embed=self.check(cfg))
             case ("set", *params):
-                await ctx.send(await self.update_val(cfg, ctx.guild.id, params))
+                await ctx.send(embed=await self.update_val(cfg, ctx.guild.id, params))
             case ("help", *params):
                 await ctx.send(embed=self.help(cfg, params))
             case _:
                 await ctx.send(embed=self.help(cfg, ()))
 
-    def check(self, cfg: GuildConfig) -> str:
+    def check(self, cfg: GuildConfig) -> ReplyEmbed:
         b = "### Config Check\n"
         err_count = 0
 
@@ -59,13 +58,14 @@ class ConfigCommand(commands.Cog):
         b += "### Summary\n"
         b += f"{err_count} issues detected. ;;toromi will run "
         b += "in a degraded state." if err_count else "as expected."
+        embed = ReplyEmbed(description=b) if err_count else ReplyEmbed.Error(description=b)
 
-        return b
+        return embed
 
     def help(self, cfg: GuildConfig, args) -> ReplyEmbed:
         args = args[0].lower() if args else ""
 
-        _embed = ReplyEmbed(description=" ", title="admin config menu")
+        _embed = ReplyEmbed(description=" ", title="𝒶𝒹𝓂𝒾𝓃 𝒸𝑜𝓃𝒻𝒾𝑔 𝓂𝑒𝓃𝓊")
 
         if args == "":
             _embed.description = "use with args below to show more info"
@@ -84,8 +84,8 @@ class ConfigCommand(commands.Cog):
 
         return _embed
 
-    async def update_val(self, cfg: GuildConfig, guild_id: int, args: list[str]) -> str:
-        # TODO: reimplement the embed that hoog did
+    async def update_val(self, cfg: GuildConfig, guild_id: int, args: list[str]) -> ReplyEmbed:
+        # TODO: reimplement the embed that hoog did # hoog: hi :3
         report = "FIXME(kajo): this message should have been overwritten"
 
         match args:
@@ -98,24 +98,29 @@ class ConfigCommand(commands.Cog):
                     setattr(cfg, field, maybe_int(value))
                     report = f"set cfg.{field} to {value}"
                 except AttributeError:
-                    return f"{field} is not a property of GuildConfig"
+                    return ReplyEmbed.Error(
+                            description=f"{field} is not a property of GuildConfig"
+                            )
 
             case [feat, field, value]:
                 try:
                     setattr(getattr(cfg, feat), field, maybe_int(value))
                     report = f"set cfg.{feat}.{field} to {value}"
                 except AttributeError:
-                    return f"{feat}.{field} is not a property of GuildConfig "
+                    return ReplyEmbed.Error(
+                        description=f"{feat}.{field} is not a property of GuildConfig"
+                    )
 
             case _:
-                return (
+                return ReplyEmbed.Error(
+                    description=
                     "To use ;;config set, either provide:\n"
                     "`;;config set foo bar` for `cfg.foo = bar`, or\n"
                     "`;;config set foo bar baz` for `cfg.foo.bar = baz`"
                 )
 
         await Config.save(guild_id)
-        return report
+        return ReplyEmbed(description=report)
 
 
 def maybe_int(s: str) -> str | int:
